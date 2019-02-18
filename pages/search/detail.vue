da0f72702df75d5449a8090812a4c8194c284436
Ryan Mason <ryanmasonjar@gmail.com>
2019-02-18 11:40:48 -0500

    Clean up detail page

--- pages/search/detail.vue
+++ pages/search/detail.vue
@@ -4,26 +4,40 @@
       <div class="card-body">
         <h2>Ocean sceicne report 2018</h2>
         <hr>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor at auctor urna nunc id cursus metus. Aliquam sem et tortor consequat id porta nibh. Pellentesque nec nam aliquam sem et tortor consequat id. Viverra maecenas accumsan lacus vel.</p>
         <div class="bd-example">
           <dl class="row">
-            <dt class="col-sm-3">DOI</dt>
-            <dd class="col-sm-9"> https://doi.org/10.1109/5.771073)</dd>
+            <dt class="col-sm-2">DOI:</dt>
+            <dd class="col-sm-10"><a href="https://doi.org/10.1109/5.771073">https://doi.org/10.1109/5.771073</a></dd>
 
-            <dt class="col-sm-3">Author</dt>
-            <dd class="col-sm-9">
+            <dt class="col-sm-2">Author:</dt>
+            <dd class="col-sm-10">
               N. Paskin 
             </dd>
 
-            <dt class="col-sm-3">Abstract</dt>
-            <dd class="col-sm-9">
+            <dt class="col-sm-2">Abstract:</dt>
+            <dd class="col-sm-10">
               <p>
                 This paper discusses the creation and use of unique identifiers for intellectual property. General concepts applicable to unique identifiers are defined and discussed [identifier, digital object, dumb and intelligent identifiers, readability, affordance or computability, multiple identification, resolution, metadata, persistence, granularity, derivatives (e.g., versions, formats, manifestations, and copies), check digits, and intermediate objects]. Requirements for unique identifiers are reviewed. Capacity issues for an identifier scheme and business issues (cost, antitrust considerations, and intellectual property rights) are explored. Technical and administrative issues of identifiers are discussed, with particular reference to the uses of identifiers, which necessitates intelligence within a system of unique identifiers (scope, protocol independence, multiple roles, fungibility, persistence, standards, and emerging structural metadata approaches). Two brief illustrations of failure in unique identifiers are given. The role of unique identifiers on the Internet is discussed with explanation of the architecture of uniform resource addressing, specifying the resource from a uniform resource name (URN), names and addresses, URN implementations, and a future digital object infrastructure. Brief examples of unique information identifier systems in music, text, and digital objects in general are discussed. Recommendations are made for actions to assist in the development of future identifiers.
               </p>
             </dd>
 
-            <dt class="col-sm-3 text-truncate">Truncated term is truncated</dt>
-            <dd class="col-sm-9">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</dd>
+            <dt class="col-sm-2 text-truncate">Date added:</dt>
+            <dd class="col-sm-10">January 1, 2018</dd>
+            <dt class="col-sm-2 text-truncate">Reference:</dt>
+            <dd class="col-sm-10">
+              <code>
+                <pre>
+"providers.gov.pubmedcentral": {
+        "journal": "Indian Journal of Urology : IJU : Journal of the Urological Society of India",
+        "in_print": "This work appeared in volume 33 issue 1 from pages 90 - 90.",
+        "copyright": "Copyright: © 2018 Indian Journal of Urology",
+        "copyright_year": "2018",
+        "correspondence": "person@gmail.com"
+    }
+}
+                </pre>
+              </code>
+            </dd>
           </dl>
         </div>
       </div>
